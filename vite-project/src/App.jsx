d94bf0d648d5b3bd5229b01640906990cf6e8d49
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2024-12-29 12:47:18 +0330

    install react-icons

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-blue-700 text-2xl'>App</div>
+    <div className='flex flex-col items-center justify-center w-screen h-screen'>
+     App
+    </div>
   )
 }
 
