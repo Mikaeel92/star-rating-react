d5b1076f6677f7e66118e132403ffc679be0df6e
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2024-12-29 12:58:20 +0330

    import  & show FaStar from react-icons

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa'
 
 const App = () => {
   return (
     <div className='flex flex-col items-center justify-center w-screen h-screen'>
-     App
+      <div className='flex gap-2'>
+      {
+      [...Array(5)].map((item, index) => {
+        index += 1
+        return <FaStar 
+        key={index}
+        size={40}
+        className='text-gray-400'
+        />
+      })}
+      </div>
     </div>
   )
 }
